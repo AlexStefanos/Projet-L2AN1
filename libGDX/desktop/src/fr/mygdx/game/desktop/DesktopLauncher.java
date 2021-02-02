d4de17975ed84a7529145f69846207afbcc137cb
root <root@DESKTOP-FHVJVBE.localdomain>
2021-02-02 14:49:14 +0100

    1er Push

---
+++ libGDX/desktop/src/fr/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.mygdx.game.ProjetL2AN1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjetL2AN1(), config);
+	}
+}
