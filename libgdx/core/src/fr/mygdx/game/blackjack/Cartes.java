ea84566345b9f72538f69c052950f70319b69b91
Ibejunior <samuel@DESKTOP-HDRC11T.localdomain>
2021-03-20 15:49:14 +0100

    Modif code BJ

--- libgdx/core/src/fr/mygdx/game/blackjack/Cartes.java
+++ libgdx/core/src/fr/mygdx/game/blackjack/Cartes.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Cartes {
 
 public ArrayList<Integer> mains;
+public ArrayList<String> mainsnom;
 public int nbparticipants;
 public Deck paquet;
 public boolean tirer;
@@ -12,6 +13,7 @@ public boolean tirer;
 	public Cartes(int nbparticipants, Deck paquet) {
 		
 		mains = new ArrayList<Integer>(5);
+		mainsnom = new ArrayList<String>(5);
 		this.nbparticipants = nbparticipants;
 		this.paquet = paquet;
 		tirer = true;
@@ -40,12 +42,23 @@ public boolean tirer;
 		
 	        for (int i =0;i<2;i++) {
 		        mains.add(i, paquet.getPaquet().get(0));
-	    	    System.out.println("Votre main : " + mains + "\n");
+		        mainsnom.add(i, paquet.getPaquetNom().get(0));
+	    	    
 	    	    paquet.getPaquet().remove(0);
+	    	    paquet.getPaquetNom().remove(0);
 	        }
+	        System.out.println("Votre main : " + mainsnom );
 	 }
+	 /*public void maindepnom() {
+			
+	        for (int i =0;i<2;i++) {
+		        mainsnom.add(i, paquet.getPaquetNom().get(0));
+	    	    System.out.println("Votre main : " + mainsnom + "\n");
+	    	    paquet.getPaquetNom().remove(0);
+	        }
+	 }*/
 	        
-	        public void tirerjoueur(ArrayList<Integer> maincroupier, int totalcroupier) {
+	        public void tirerjoueur(ArrayList<String> maincroupier, int totalcroupier) {
 	            while (tirer && total() < 21) {
 	        	/*String reponse;
 	    	    Scanner clavier = new Scanner(System.in);
@@ -53,11 +66,13 @@ public boolean tirer;
 	    	    reponse = clavier.nextLine();*/
 	        	    int a;
 	        	    
-	        	    a = Saisie.lireEntier("\n Voulez vous tirer une carte, sachant que la main du croupier est " +  maincroupier  + "donc un total de " + totalcroupier +  " ?");
+	        	    a = Saisie.lireEntier("\n Voulez vous tirer une carte, sachant que la main du croupier est [" +  maincroupier.get(0) +  ", ?]");
 	    	        if (a == 1) {
+	    	        mainsnom.add(paquet.getPaquetNom().get(0));
 	    	        mains.add(paquet.getPaquet().get(0));
+	    	        paquet.getPaquetNom().remove(0);
 	    	        paquet.getPaquet().remove(0);
-	    	        System.out.print("\nVotre main : " + mains + "\n");
+	    	        System.out.print("\nVotre main : " + mainsnom + "\n");
 	    	        }
 	    	        if (a != 1) {
 	    	    	    tirer = false;
@@ -66,23 +81,43 @@ public boolean tirer;
 	        
 	       
 	 }
-	 public ArrayList<Integer> croupierdep() {
+	 public ArrayList<String> croupierdep() {
 		 for (int i =0;i<2;i++) {
 		        mains.add(i, paquet.getPaquet().get(0));
-	    	    System.out.println("\nMain du croupier : " + mains + "\n");
+		        mainsnom.add(i, paquet.getPaquetNom().get(0));
+	    	    
 	    	    paquet.getPaquet().remove(0);
+	    	    paquet.getPaquetNom().remove(0);
 		 }
-		 return mains;
+		 System.out.println("\nMain du croupier : " + mainsnom.get(0) + " une autre carte face cachée " );
+		 return mainsnom;
 		 
 	 }
+	 
+	/* public ArrayList<String> croupierdepNom() {
+		 for (int i =0;i<2;i++) {
+		        mainsnom.add(i, paquet.getPaquetNom().get(0));
+	    	    System.out.println("\nMain du croupier : " + mainsnom + "\n");
+	    	    paquet.getPaquetNom().remove(0);
+		 }
+		 return mainsnom;
+		 
+	 }*/
+
 	 public ArrayList<Integer> afficheMainCroupier(){
 		 return mains;
-		 
+	 }
+	public ArrayList<String> afficheMainCroupierNom(){
+			 return mainsnom;
 	 }
 	 public void croupiertirer(int totaljoueur) {
 		 while (total() < 17) {
 			 mains.add(paquet.getPaquet().get(0));
-			 System.out.println("\nMain du croupier après tirage : " + mains + "\n");
+			 mainsnom.add(paquet.getPaquetNom().get(0));
+			 //mainsnom.add(paquet.getPaquetNom().get(0));
+			 paquet.getPaquet().remove(0);
+			 paquet.getPaquetNom().remove(0);
+			 System.out.println("\nMain du croupier après tirage : " + mainsnom + "\n");
 		 }
 		 if (total() > 21 && totaljoueur < 22 ) {
 			 System.out.println("\nLe croupier à un score de " + total() + " votre score est de " + totaljoueur + " donc êtes vainqueurs");
@@ -93,7 +128,7 @@ public boolean tirer;
 		 else if (totaljoueur > 21 ) {
 			 System.out.println("Votre total est de " + totaljoueur + " donc vous avez perdu");
 		 }
-		 else if (total() < 22 && total() < totaljoueur && totaljoueur < 22) {
+		 else if (total() < 22 && total() < totaljoueur && totaljoueur < 22  ) {
 			 System.out.println("Vous avez un score de " + totaljoueur + " et le croupier un score de " + total() + " vous êtes donc vainqueurs");
 		 }
 		 else if (total() < 22 && total() > totaljoueur && totaljoueur < 22) {
@@ -102,13 +137,19 @@ public boolean tirer;
 	 }
 	 public int total() {
 		 int somme = 0;
+		 boolean flag = false;
 		 for (int i = 0;i<mains.size();i++) {
+			 
+			 if (mains.get(i) == 1) {
+				 flag = true;
+			 }
 			 somme +=  mains.get(i);
 		 }
-		 //System.out.println("\nVotre main vaut " + somme );
-		 return somme;
-			 
-	 }
+		 if (flag && 10+somme <= 21) {
+			 somme += 10;
+		 }
+		return somme;
+	}
 	public void conversion() {
 		for (int i = 0; i<312;i++) {
 			if (paquet.getPaquet().get(i) > 10) {
--- libgdx/core/src/fr/mygdx/game/blackjack/TestCartes.java
+++ libgdx/core/src/fr/mygdx/game/blackjack/TestCartes.java
@@ -1,9 +1,11 @@
 package fr.mygdx.game.blackjack;
+import java.util.ArrayList;
 
 public class TestCartes {
 	public static void main(String[] args) {
     	int a = Saisie.lireEntier("Nombre de joueurs à la table ? \n");
 		Deck paquet = new Deck();
+		Deck paquetnom = new Deck();
     	paquet.generateur();
     	
     	Cartes p1 = new Cartes(a, paquet);
@@ -11,16 +13,25 @@ public class TestCartes {
     	Cartes p3 = new Cartes(a, paquet);
     	Cartes p4 = new Cartes(a, paquet);
     	Cartes croupier = new Cartes(a, paquet);
+    	Cartes croupiernom = new Cartes(a, paquet);
     	
-    	paquet.conversion();
+    	
     	paquet.shuffle();
-    	//paquet.afficher();
-    	croupier.croupierdep();
+    	paquetnom = paquet;
+    	paquet.conversion();
+    	paquetnom.creation();
+    	paquetnom.affichernoms();
+    	
+    	paquet.afficher();
+    	ArrayList<String> b = croupier.croupierdep();
+    	//croupiernom.croupierdepNom();
     	p1.maindep();
+    	//p1.maindepnom();
     	//p2.maindep();
     	//p3.maindep();
     	//p4.maindep();
-    	p1.tirerjoueur(croupier.afficheMainCroupier(),croupier.total());
+    	p1.tirerjoueur(b,croupier.total());
+    	//p1.tirerjoueur(croupier.afficheMainCroupier(),croupier.total());
     	//p2.tirerjoueur(croupier.afficheMainCroupier(),croupier.total());
     	//p3.tirerjoueur(croupier.afficheMainCroupier(),croupier.total());
     	//p4.tirerjoueur(croupier.afficheMainCroupier(),croupier.total());
